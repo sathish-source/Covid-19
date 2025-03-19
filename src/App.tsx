e07172c1481231bd96255c96270273732aac8634
Sathish <sathishsaga33@gmail.com>
2025-03-19 11:02:25 -0700

    finalResponsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
 import './App.css'
-import Footer from './Compond/Footer';
-import About from './Compond/Pages/About';
-import Contact from './Compond/Pages/Contact';
-import Home from './Compond/Pages/Home';
-import Navbar from './Compond/Navbar';
-
+import Footer from './Components/Footer';
+import About from './Components/Pages/About';
+import Contact from './Components/Pages/Contact';
+import Home from './Components/Pages/Home';
+import Navbar from './Components/Navbar';
 import { BrowserRouter,  Route, Routes,  } from 'react-router-dom'
-import FAQ from './Compond/Pages/FAQ';
-import GenaralInfo from './Compond/Pages/GenaralInfo';
-import News from './Compond/Pages/News';
-// import Nav from './Componants/Navbar/Nav';
-// import CovidHome from './Componants/Home/Covidhome';
-// import About from './Componants/About/About'
-// import Contact from './Componants/Contact';
-// import Footer from './Componants/Footer/Footer';
-// import FAQ from './Componants/FAQ';
-// import General from './Componants/GeneralInformation';
-// import News from './Componants/News';
+import FAQ from './Components/Pages/FAQ';
+import GenaralInfo from './Components/Pages/GenaralInfo';
+import News from './Components/Pages/News';
+
 
 function App() {
-
-
   return (
     <div className="app">
-
       <BrowserRouter>
        <Navbar />
       <Routes>
@@ -36,23 +25,6 @@ function App() {
       </Routes>
       <Footer/>
       </BrowserRouter>
-     
-
-
-      {/* <BrowserRouter>
-          <Nav/>
-          <Routes>
-          <Route path='/' element={ < CovidHome/> }/>
-          <Route path='/About' element={<About/> }/>
-          <Route path='/Contact' element={<Contact/>}/>
-          <Route path='/FAQ' element={<FAQ/>}/>
-          <Route path='/GeneralInformation' element={<General/>}/>
-          <Route path='/News' element={<News/>}/>
-          </Routes>
-          <Footer/>
-      </BrowserRouter>
-      */}
-
     </div>
 
   )
